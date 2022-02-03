8a1c2636bc44018fd7fa395fc3fc7a067a29b809
Leônidas Borges Caetano <lborgescaetano@gmail.com>
2022-02-03 09:17:31 -0300

    primeiro commit

---
+++ js/debounce.js
@@ -0,0 +1,10 @@
+export default function debounce(callback, delay) {
+  let timer;
+  return (...args) => {
+    if (timer) clearInterval(timer);
+    timer = setInterval(() => {
+      callback(...args);
+      timer = null;
+    }, delay)
+  }
+}
